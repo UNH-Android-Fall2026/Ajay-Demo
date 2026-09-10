ae30212798af7121eaf4052642d9799cfa6fae6e
fpellicano <fpellicano@newhaven.edu>
2026-09-10 19:55:36 -0400

    Drawing view, added string resources

--- app/src/main/java/com/unh/professorpellicanoicebreaker/MainScreen.kt
+++ app/src/main/java/com/unh/professorpellicanoicebreaker/MainScreen.kt
@@ -3,10 +3,17 @@ package com.unh.professorpellicanoicebreaker
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -20,9 +27,29 @@ fun MainScreen() {
         horizontalAlignment = Alignment.CenterHorizontally
     ){
         Text(
-            text = "Icebreaker!",
-            fontSize = 36.sp
+            text = stringResource(R.string.header_text),
+            fontSize = 36.sp,
+            fontWeight = FontWeight.Bold
         )
+        Text(
+            text = stringResource(R.string.header_sub_text),
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Normal
+        )
+
+        var firstName by remember { mutableStateOf("") }
+
+        OutlinedTextField(
+            enabled = true,
+            singleLine = true,
+            modifier = Modifier.padding(top = 50.dp),
+            value = firstName,
+            onValueChange = {firstName = it},
+            label = {
+                Text(stringResource(R.string.first_name_text))
+            }
+        )
+
     }
 }
 
