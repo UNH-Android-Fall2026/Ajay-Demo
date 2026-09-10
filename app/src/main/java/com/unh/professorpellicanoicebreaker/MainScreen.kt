05bcf67c09a5793f23b5eaf91740aa6b5afb9e29
fpellicano <fpellicano@newhaven.edu>
2026-09-10 19:42:19 -0400

    Initial View Commit

---
+++ app/src/main/java/com/unh/professorpellicanoicebreaker/MainScreen.kt
@@ -0,0 +1,33 @@
+package com.unh.professorpellicanoicebreaker
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun MainScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 100.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(
+            text = "Icebreaker!",
+            fontSize = 36.sp
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview(){
+    MainScreen()
+}
