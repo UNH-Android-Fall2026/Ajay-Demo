daa580bcdd329f01bcdf3960830e86155c97fbd9
fpellicano <fpellicano@newhaven.edu>
2026-09-10 20:23:46 -0400

    Created question model

--- app/src/main/java/com/unh/professorpellicanoicebreaker/MainActivity.kt
+++ app/src/main/java/com/unh/professorpellicanoicebreaker/MainActivity.kt
@@ -10,7 +10,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import com.unh.professorpellicanoicebreaker.ui.theme.ProfessorPellicanoIcebreakerTheme
 
-// Test Commit
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/unh/professorpellicanoicebreaker/Questions.kt
@@ -0,0 +1,6 @@
+package com.unh.professorpellicanoicebreaker
+
+data class Questions(
+    val text: String = "", // Question content from our database
+    val id: String = "" // Unique hash identifier within database
+)
